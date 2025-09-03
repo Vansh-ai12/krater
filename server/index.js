8f18ea4984a6aeb9fcc2602c7d02a8c15763ad28
Vansh Jain <vj2754108@gmail.com>
2025-09-04 00:07:17 +0530

    API setup for  sending code to backend is done!!

--- server/index.js
+++ server/index.js
@@ -1,8 +1,8 @@
 const express = require("express");
 const PORT = 8000;
-
+const cors = require('cors');
 const mysql = require('mysql2');
-
+let latestCode ={};
 const pool = mysql.createPool({
   host: 'localhost',
   user: 'krateruser',
@@ -18,11 +18,40 @@ const pool = mysql.createPool({
 });
 
 const app = express();
-
-
+app.use(express.json()); 
+app.use(
+  cors(
+    {
+      origin:"http://localhost:3000",
+      credentials: true
+    }
+  )
+)
 app.get("/", (req, res) => {
   return res.end("HomePAGE");
 });
+app.post("/code", (req, res) => {
+  const { lang, code } = req.body;
+
+
+  latestCode = { lang, code };
+
+  return res.status(201).json({
+    success: true,
+    data: latestCode
+  });
+});
+
+app.get("/code", (req, res) => {
+  if (!latestCode) {
+    return res.status(404).json({ message: "No code has been submitted yet" });
+  }
+
+  return res.json({
+    data: latestCode
+  });
+});
+
 
 app.get("/data", (req, res) => {
   pool.query("SELECT * FROM users", (err, results) => {
