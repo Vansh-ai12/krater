686949a40042134d1527432c175b1640783e57a2
Vansh Jain <vj2754108@gmail.com>
2025-09-11 01:04:03 +0530

    jb

--- client/lib/kinde.js
+++ client/lib/kinde.js
@@ -3,7 +3,7 @@ import { KindeAuth } from "@kinde-oss/kinde-auth-js";
 export const auth = new KindeAuth({
   clientId: process.env.NEXT_PUBLIC_KINDE_CLIENT_ID,
   redirectUri:
-    "https://krater-ndioeokdd-vansh-jains-projects-c99da508.vercel.app/api/auth/kindeauth",
+    "https://krater-4qvidtgb9-vansh-jains-projects-c99da508.vercel.app/api/auth/kindeauth",
   storage: "localStorage", // ← fixes state mismatch
   onRedirectCallback: (appState) => {
     window.location.href = appState?.targetUrl || "/dashboard";
